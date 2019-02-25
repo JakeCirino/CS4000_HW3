67428c0c13504d9ccae7308ab0b5be3b9341d382
Jake Cirino <jc767615@ohio.edu>
2019-02-25 11:04:30 -0500

    Neighbor calculation added, needs testing

--- conway.c
+++ conway.c
@@ -2,7 +2,8 @@
 #include <vector>
 
 vector<vector<int>> SimulateLife(vector<vector<int>> &board, int life_cycles){
-    for(int lc = 0; lc < life_cycles; _LOCK_AT_THREAD_EXIT++){
+    //loop through for the number of life cycles
+    for(int lc = 0; lc < life_cycles; lc++){
         //initiate new board
         vector<vector<int>> new_board;
         new_board.resize(board.size());
@@ -13,7 +14,8 @@ vector<vector<int>> SimulateLife(vector<vector<int>> &board, int life_cycles){
         for(int i = 0; i < iterations; i++){
             int x = GetX(i, board.size()),
                 y = GetY(i, board.size());
-            
+            if(board[x][y] == 2)
+                new_board[x][y] = 2;
         }
     }
 }
@@ -26,6 +28,32 @@ int GetY(int index, int size){
     return floor(index/size);
 }
 
-int[] GetNeighbors(vector<vector<int>> &board, int x, int y){
-    //TODO
+int ToIndex(int x, int y, int size){
+    return x + y*size;
+}
+
+int Wrap(int val, int size){
+    if(val < 0)
+        return size;
+    else if(val >= size)
+        return 0;
+    else
+        return val;
+}
+
+int NeighborsAlive(vector<vector<int>> &board, int x, int y){
+    int alive = 0;
+    int size = board.size();
+    for(int x1 = x-1; x1 <= x+1; x1++){
+        for(int y1 = y - 1; y1 <= y+1; y1++){
+            //get actual values in case we need to wrap around the world
+            int actual_x = Wrap(x1, size);
+            int actual_y = Wrap(y1, size);
+            
+            //check if the value is alive or dead
+            if(board[actual_x][actual_y] != 0)
+                alive++;
+        }
+    }
+    return alive;
 }
--- conway.h
+++ conway.h
@@ -9,7 +9,11 @@ private:
     int GetX(int index, int size);
     //gets the y location based off the index/size of the world
     int GetY(int index, int size);
-    //returns an array of the neighbors of an index
-    int[] GetNeighbors(vector<vector<int>> &board, int x, int y);
+    //returns coordinates to an index
+    int ToIndex(int x, int y, int size);
+    //wraps coordinate values based on width/height
+    int Wrap(int val, int size);
+    //returns the number of neighbors alive for a given square
+    int NeighborsAlive(vector<vector<int>> &board, int x, int y);
 };
 #endif
