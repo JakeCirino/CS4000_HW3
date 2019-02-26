e534f779028c2965053459110f48514c59b51bd1
Jake Cirino <jc767615@ohio.edu>
2019-02-26 15:06:14 -0500

    Attempted to fix board seg faults

--- GameOfLife.cc
+++ GameOfLife.cc
@@ -42,9 +42,15 @@ int GameOfLife::NeighborsAlive(vector<vector<int>> &board, int x, int y){
 }
 
 vector<vector<int>> GameOfLife::SimulateLife(vector<vector<int>> &board, int life_cycles){
+    //create the current board
+    vector<vector<int>> current_board;
+    current_board.resize(board.size());
+    for(int i = 0; i < board.size(); i++){
+        current_board[i].resize(board.size());
+    }
+    current_board = board;
+
     //loop through for the number of life cycles
-    vector<vector<vector<int>>> boards;
-    boards.push_back(board)
     for(int lc = 0; lc < life_cycles; lc++){
         //initiate new board
         vector<vector<int>> tmp_board;
@@ -61,11 +67,11 @@ vector<vector<int>> GameOfLife::SimulateLife(vector<vector<int>> &board, int lif
                 y = GetY(i, board.size());
 
             //calculate if cell should be alive or dead
-            if(board[x][y] != 2){
-                int alive = NeighborsAlive(board, x, y);
+            if(current_board[x][y] != 2){
+                int alive = NeighborsAlive(current_board, x, y);
                 if(alive >= 4 || alive <= 1){
                     tmp_board[x][y] = 0;
-                }else if(board[x][y] == 1 && (alive == 2 || alive == 3)){
+                }else if(current_board[x][y] == 1 && (alive == 2 || alive == 3)){
                     tmp_board[x][y] = 1;
                 }else if(alive == 3){
                     tmp_board[x][y] = 1;
@@ -73,7 +79,7 @@ vector<vector<int>> GameOfLife::SimulateLife(vector<vector<int>> &board, int lif
             }else
                 tmp_board[x][y] = 2;
         }
-        board = tmp_board;
+        current_board = tmp_board;
     }
-    return board;
+    return current_board;
 }
