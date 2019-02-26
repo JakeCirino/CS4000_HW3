9b84e419c3cfbf9c18c9a4d6e6b1c60bf36fbe1b
Jake Cirino <jc767615@ohio.edu>
2019-02-26 16:46:27 -0500

    Remove single loop in place of nested loop

--- GameOfLife.cc
+++ GameOfLife.cc
@@ -4,18 +4,6 @@
 #include <iostream>
 using namespace std;
 
-int GameOfLife::GetX(int index, int size){
-    return floor(index/size);
-}
-
-int GameOfLife::GetY(int index, int size){
-    return index%size;
-}
-
-int GameOfLife::ToIndex(int x, int y, int size){
-    return x + y*size;
-}
-
 int GameOfLife::Wrap(int val, int size){
     if(val < 0)
         return size-1;
@@ -25,19 +13,19 @@ int GameOfLife::Wrap(int val, int size){
         return val;
 }
 
-int GameOfLife::NeighborsAlive(vector<vector<int>> &board, int x, int y){
+int GameOfLife::NeighborsAlive(vector<vector<int>> &board, int i, int j){
     int alive = 0;
     int size = board.size();
-    for(int x1 = x - 1; x1 <= x+1; x1++){
-        for(int y1 = y - 1; y1 <= y+1; y1++){
+    for(int i1 = i - 1; i1 <= i+1; i1++){
+        for(int j1 = j - 1; j1 <= j+1; j1++){
             //get actual values in case we need to wrap around the world
-            int actual_x = Wrap(x1, size);
-            int actual_y = Wrap(y1, size);
+            int actual_i = Wrap(i1, size);
+            int actual_j = Wrap(j1, size);
             
             //cout << actual_x << ", " << actual_y << ": " << board[actual_x][actual_y] << endl;
 
             //check if the value is alive or dead
-            if(board[actual_x][actual_y] != 0 && actual_x != x && actual_y != y)
+            if(board[actual_i][actual_j] != 0 && actual_i != i && actual_j != j)
                 alive++;
         }
     }
@@ -60,22 +48,21 @@ vector<vector<int>> GameOfLife::SimulateLife(vector<vector<int>> &board, int lif
         //store iterations
         int iterations = board.size()*board.size();
         //iterate through board
-        for(int i = 0; i < iterations; i++){
-            int x = GetX(i, board.size()),
-                y = GetY(i, board.size());
-
-            //calculate if cell should be alive or dead
-            if(current_board[x][y] != 2){
-                int alive = NeighborsAlive(current_board, x, y);
-                if(alive >= 4 || alive <= 1){
-                    tmp_board[x][y] = 0;
-                }else if(current_board[x][y] == 1 && (alive == 2 || alive == 3)){
-                    tmp_board[x][y] = 1;
-                }else if(alive == 3){
-                    tmp_board[x][y] = 1;
-                }
-            }else
-                tmp_board[x][y] = 2;
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board.size(); j++){
+                //calculate if cell should be alive or dead
+                if(current_board[i][j] != 2){
+                    int alive = NeighborsAlive(current_board, i, j);
+                    if(alive >= 4 || alive <= 1){
+                        tmp_board[i][j] = 0;
+                    }else if(current_board[i][j] == 1 && (alive == 2 || alive == 3)){
+                        tmp_board[i][j] = 1;
+                    }else if(alive == 3){
+                        tmp_board[i][j] = 1;
+                    }
+                }else
+                    tmp_board[i][j] = 2;
+            }   
         }
         current_board = tmp_board;
     }
--- GameOfLife.h
+++ GameOfLife.h
@@ -7,15 +7,9 @@ public:
     std::vector<std::vector<int>> SimulateLife(std::vector<std::vector<int>> &board, int life_cycles);
 
 private:
-    //gets the x location based off the index/size of the world
-    int GetX(int index, int size);
-    //gets the y location based off the index/size of the world
-    int GetY(int index, int size);
-    //returns coordinates to an index
-    int ToIndex(int x, int y, int size);
     //wraps coordinate values based on width/height
     int Wrap(int val, int size);
     //returns the number of neighbors alive for a given square
-    int NeighborsAlive(std::vector<std::vector<int>> &board, int x, int y);
+    int NeighborsAlive(std::vector<std::vector<int>> &board, int i, int j);
 };
 #endif
